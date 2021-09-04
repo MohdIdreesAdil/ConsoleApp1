84a33ec34b3eed3d8866ae47eda1d8ef3d452f07
Lenovo <Lenovo@LAPTOP-4N2U9Q2V>
2021-09-04 15:37:20 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String name;
+            Console.WriteLine("**********Please Enter Your Name*********");
+            name = Console.ReadLine();
+            Console.WriteLine("***Wecome To C# and DotNet Course **"+name+"**");
+            
+            //Addition of two numbers
+            int num1 = 10;
+            int num2 = 20;
+            int c = num1 + num2;
+            Console.WriteLine("Addition is = " + c);
+            Console.WriteLine("The addition of {0} and {1} is = {2}", num1, num2, c);
+            // user input 
+            int x, y, z;
+            Console.Write("Enter the first number = ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the second number = ");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            z = x + y;
+            Console.WriteLine("addition of numbers = "+z);
+
+        }
+    }
+}
+
